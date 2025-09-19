cc6b35806d65061e019d65d92d92bf37c166577a
RicardoOcampo06 <20243ds169@utez.edu.mx>
2025-09-19 11:14:13 -0600

    Este es el trabajo de Ricardo Naul

---
+++ SUMAR/src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static int suma(int n){
+        if (n ==0 ) return 0;
+        
+        return n + suma(n - 1);
+    }
+    public static void main(String[] args) throws Exception {
+        int n = 5;
+
+        System.out.println("La suma es la siguiente:");
+
+        for(int i = 0; i <= n ; i++){
+            System.out.print( i + " ");
+
+        }
+       System.out.println(" = "+ suma(n));
+    }
+}
+
